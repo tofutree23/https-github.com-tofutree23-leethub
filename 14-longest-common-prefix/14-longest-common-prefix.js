987a7dadc3fbbfb8828923ea97d86c6d8fedfa7e
luke <tofutree23@gmail.com>
2022-04-14 14:14:04 +0900

    Time: 77 ms (67.77%), Space: 44.7 MB (11.87%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.js
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -3,17 +3,17 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-    if (!strs.length) return ''
+    let prefix = ''
+    if (strs.length === 0) return prefix
     
     const firstWord = strs[0]
     
     for (let i = 0; i < firstWord.length; i++) {
         for (const str of strs) {
-            if (str[i] !== firstWord[i]) {
-                return str.slice(0, i)
-            }
+            if (str[i] !== firstWord[i]) return prefix
         }
+        prefix += firstWord[i]
     }
-
-    return firstWord
+    
+    return prefix
 };
